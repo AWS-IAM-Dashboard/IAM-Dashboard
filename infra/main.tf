dce85a35a8240c2387928d1b5bdb6540add25c01
Alvin <alvinjanton575@gmail.com>
2026-03-31 11:42:47 -0400

    Fixed terraform formatting error

--- infra/main.tf
+++ infra/main.tf
@@ -90,6 +90,12 @@ module "api_gateway" {
   environment  = var.environment
   project_name = var.project_name
   kms_key_arn  = aws_kms_key.IAM_Dashboard_Key.arn
+  aws_region            = var.aws_region
+  environment           = var.environment
+  project_name          = var.project_name
+  kms_key_arn           = data.aws_kms_key.logs.arn
+  cognito_issuer_url    = module.cognito.issuer_url
+  cognito_app_client_id = module.cognito.app_client_id
 }
 
 # API Gateway Module for the Authentication APIs
